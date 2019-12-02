6bec630bd3b6b5b9c9809c0d021223b2a32fe360
mehmet-erim <bilgi@mehmeterim.com>
2019-12-02 16:39:12 +0300

    add svelte web component

---
+++ src/svelte/rollup.config.js
@@ -0,0 +1,56 @@
+import replace from 'rollup-plugin-re';
+import svelte from 'rollup-plugin-svelte';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import livereload from 'rollup-plugin-livereload';
+import { terser } from 'rollup-plugin-terser';
+import scss from 'rollup-plugin-scss';
+
+const production = !process.env.ROLLUP_WATCH;
+
+export default {
+  input: 'src/main.js',
+  output: {
+    sourcemap: !production,
+    format: 'iife',
+    name: 'app',
+    file: 'public/counter.js',
+  },
+  plugins: [
+    svelte({
+      dev: !production,
+      // Tell the compiler to output a custom element.
+      customElement: true,
+    }),
+    // If you have external dependencies installed from
+    // npm, you'll most likely need these plugins. In
+    // some cases you'll need additional configuration —
+    // consult the documentation for details:
+    // https://github.com/rollup/rollup-plugin-commonjs
+    resolve(),
+    replace({
+      patterns: [
+        {
+          test: /document\.head\.appendChild\(style\)/,
+          replace: 'find_root(node).appendChild(style)',
+        },
+        {
+          test: /function noop\(\)/,
+          replace: `function find_root(element) {
+            const parent = element.parentNode;
+            return parent ? (parent.head ? parent.head : find_root(parent)) : element;
+          }
+            function noop()`,
+        },
+      ],
+    }),
+    commonjs(),
+
+    // Enable live reloading in development mode
+    !production && livereload('public'),
+
+    // Minify the production build (npm run build)
+    production && terser(),
+    scss(),
+  ],
+};
---
+++ src/svelte/src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+
+</script>
+
+<style>
+
+</style>
+
+<!-- 
+This tells the Svelte compiler that this file is a custom element. 
+We also have to include the "customElement: true" compiler setting in rollup configuration.
+-->
+<svelte:options tag="svelte-counter-element" />
+
+<div id="svelte-counter">
+  Svelte
+  <br />
+  0
+</div>
